3513ac34d45d47cd60bb59740df28094cf8eb0d4
Setthapong Maknoun <mr.suit@Setthapongs-MacBook-Air.local>
2024-04-26 23:32:58 +0700

    Fixed padding NavbarMd

--- src/app/components/OrganismMobile/NavberMd.jsx
+++ src/app/components/OrganismMobile/NavberMd.jsx
@@ -8,21 +8,21 @@ import Link from 'next/link';
 
 function NavberMd() {
   return (
-    <div className='max-w-screen-md bg-black flex justify-between text-white px-8 py-6'>
+    <div className='max-w-screen-md bg-black grid grid-cols-5 text-white py-3'>
         <Link href="/">    
-            <div className='flex flex-col items-center gap-1'>
+            <div className='flex flex-col items-center gap-2'>
                     <FaHouse className='w-6 h-6'/>
                     <p className='text-sm'>Home</p>
             </div>
         </Link>    
         <Link href="/">    
-            <div className='flex flex-col items-center gap-1'>
+            <div className='flex flex-col items-center gap-2'>
                     <FaInbox  className='w-5 h-6'/>
                     <p className='text-sm'>Activity</p>
             </div>
         </Link>
         <Link href="/create-room">
-            <div className='flex flex-col items-center fill-black gap-1'>
+            <div className='flex flex-col items-center fill-black gap-2'>
                     <Image 
                         src={Logo}
                         width={24}
@@ -33,7 +33,7 @@ function NavberMd() {
             </div>
         </Link>    
         <Link href="/">    
-            <div className='flex flex-col items-center gap-1'>
+            <div className='flex flex-col items-center gap-2'>
                 
                     <FaBell className='w-6 h-6'/>
                     <p className='text-sm'>Notification</p>
@@ -42,7 +42,7 @@ function NavberMd() {
         </Link>
 
         <Link href="/account-info">
-            <div className='flex flex-col items-center gap-1'>
+            <div className='flex flex-col items-center gap-2'>
                 <FaCircleUser className='w-6 h-6'/>
                 <p className='text-sm'>Account</p>
             </div>
